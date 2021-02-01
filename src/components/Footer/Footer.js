e6858281a4e8a6aa58de09da67d3b3dfe3906a02
Adrien Barbier <abarbier@debutant-e.pro>
2021-02-01 22:39:27 +0100

    Adding footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import classes from './Footer.module.scss'
+import linkedin from '../../images/icon_linkedin.svg'
+import facebook from '../../images/facebook.svg'
+import twitter from '../../images/twitter.svg'
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className={classes.footer}>
+            <div className={classes.followUs}>
+                Nous suivre sur les réseaux:
+            </div>
+            <div className={classes.socialLinks}>
+                <img src={linkedin} alt="social link linkedin" />
+                <img src={facebook} alt="social link linkedin" />
+                <img src={twitter} alt="social link linkedin" />
+            </div>
+        </footer>
     )
 }
 
