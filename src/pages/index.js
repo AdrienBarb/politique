ffc61c7cc43e04a69089d53b6e4ad87b611d0f05
Adrien Barbier <abarbier@debutant-e.pro>
2021-05-05 20:49:22 +0200

    (fix) - restart the project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,24 @@
-import * as React from "react"
+import * as React from 'react'
 import '../styles/index.scss'
 import Layout from '../components/Layout/Layout'
 import Presentation from '../components/Presentation/Presentation'
 import Information from '../components/Information/Information'
 import LastArticles from '../components/LastArticles/LastArticles'
+import { StyledConstructionSite } from '../styles/globalStyles'
 
 const IndexPage = () => {
   return (
-    <Layout>
-      <Presentation />
-      <Information />
-      <LastArticles />
-    </Layout>
+    // <Layout>
+    //   <Presentation />
+    //   <Information />
+    //   <LastArticles />
+    // </Layout>
+
+    <StyledConstructionSite>
+      Site en construction <br/>
+      - <br/>
+      Élections régionales 2021
+    </StyledConstructionSite>
   )
 }
 
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const StyledConstructionSite = styled.div`
+    margin: 8rem auto;
+    margin: 8rem auto;
+    background-color: black;
+    text-align: center;
+    font-size: 4rem;
+    padding: 2rem;
+    border-radius: 12px;
+    width: fit-content;
+    color: white;
+
+`
