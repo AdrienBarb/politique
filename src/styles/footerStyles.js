7770cf6c7fc09edb1435ee74a92871210a811298
Adrien Barbier <abarbier@debutant-e.pro>
2021-05-10 23:10:12 +0200

    (feat) - add footer + social and contact links

--- src/styles/footerStyles.js
+++ src/styles/footerStyles.js
@@ -9,4 +9,39 @@ export const StyledFooter = styled.div`
   font-size: 1.4rem;
   background-color: #323434;
   color: white;
+  font-weight: 200;
+
+  .wrapper {
+    color: white;
+    display: flex;
+    align-items: center;
+  }
+
+  .divider {
+    width: 1px;
+    height: 40px;
+    display: block;
+    background-color: white;
+    margin: 0 2rem;
+  }
+
+  .iconWrapper {
+    display: flex;
+    align-items: center;
+    margin-left: 1rem;
+  }
+
+  a {
+    display: grid;
+    place-items: center;
+  }
+
+  img {
+    width: 30px;
+    margin: 0 0.4rem;
+  }
+
+  .phoneIcon {
+    width: 32px;
+  }
 `
