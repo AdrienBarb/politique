871d618bf0c3cde65f27a6ac077a77636da8c7e9
Adrien Barbier <abarbier@debutant-e.pro>
2021-05-14 21:07:45 +0200

    (fix) - comment article file

--- src/templates/article.js
+++ src/templates/article.js
@@ -1,37 +1,37 @@
-import React from 'react'
-import { graphql } from "gatsby"
+// import React from 'react'
+// import { graphql } from "gatsby"
 
-const article = ({ data }) => {
-    return (
+// const article = ({ data }) => {
+//     return (
        
-            <div>
-                {data.allContentfulArticles.edges[0].node.titre}
-            </div>
+//             <div>
+//                 {data.allContentfulArticles.edges[0].node.titre}
+//             </div>
      
-    )
-}
+//     )
+// }
 
-export default article
+// export default article
 
 
-export const query = graphql`
-  query ($id: String!) {
-    allContentfulArticles(filter: { contentful_id: { eq: $id } }) {
-        edges {
-            node {
-            titre
-            createdAt(formatString: "DD/MM/YYYY")
-            description
-            {
-                description
-            }
-            image {
-                fluid(maxWidth: 980) {
-                    ...GatsbyContentfulFluid
-                }
-            }
-            }
-        }
-    }
-  }
-`
+// export const query = graphql`
+//   query ($id: String!) {
+//     allContentfulArticles(filter: { contentful_id: { eq: $id } }) {
+//         edges {
+//             node {
+//             titre
+//             createdAt(formatString: "DD/MM/YYYY")
+//             description
+//             {
+//                 description
+//             }
+//             image {
+//                 fluid(maxWidth: 980) {
+//                     ...GatsbyContentfulFluid
+//                 }
+//             }
+//             }
+//         }
+//     }
+//   }
+// `
