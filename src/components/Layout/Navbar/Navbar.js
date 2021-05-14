03b540265919b0b56655d5e0d768c25e30c92ddf
Adrien Barbier <abarbier@debutant-e.pro>
2021-05-14 20:19:34 +0200

    (feat) - add fullnavigation for smartphone

--- src/components/Layout/Navbar/Navbar.js
+++ src/components/Layout/Navbar/Navbar.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { StyledNavbar } from '../../../styles/navbarStyles'
+import FullNav from '../../Globals/FullNavigation'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
 
 const Navbar = () => {
+  const matches = useMediaQuery('(max-width:768px)')
   return (
     <StyledNavbar>
       <div className="navbar">
         <div className="left-links">
           <Link to="/">VoideyBarbier2021</Link>
         </div>
-        <div className="right-links">
-          <Link to="/equipe-de-campagne" activeClassName="active">
-            Qui sommes nous ?
-          </Link>
-        </div>
+        {matches ? (
+          <FullNav />
+        ) : (
+          <div className="right-links">
+            <Link to="/equipe-de-campagne" activeClassName="active">
+              Qui sommes nous ?
+            </Link>
+          </div>
+        )}
       </div>
     </StyledNavbar>
   )
