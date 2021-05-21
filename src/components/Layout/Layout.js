f036416e2151573dc05b11cc4991a4e1156cc54a
Adrien Barbier <abarbier@debutant-e.pro>
2021-05-21 09:25:59 +0200

    (fix) - fixing loading style bug

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Navbar from './Navbar/Navbar'
 import Footer from './Footer/Footer'
 import { StyledLayout } from '../../styles/layoutStyles'
+import { GlobalStyle } from '../../styles/globalStyles'
 
 const Layout = (props) => {
   return (
     <StyledLayout>
+      <GlobalStyle />
       <Navbar />
       <main>{props.children}</main>
       <Footer />
