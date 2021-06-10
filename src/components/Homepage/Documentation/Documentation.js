9cbda6d0fe091757c42e74d4a554c5e49fbffb90
Adrien Barbier <abarbier@debutant-e.pro>
2021-06-10 21:09:50 +0200

    (feat) - add new document

--- src/components/Homepage/Documentation/Documentation.js
+++ src/components/Homepage/Documentation/Documentation.js
@@ -4,6 +4,8 @@ import { StyledDocumentation } from '../../../styles/documentationStyles'
 import circulaire from '../../../images/affiche.jpeg'
 // @ts-ignore
 import lettre from '../../../images/lettre.pdf'
+// @ts-ignore
+import ensemble from '../../../images/ensemble.pdf'
 import DocumentCard from './DocumentCard'
 
 const Documentation = () => {
@@ -16,6 +18,9 @@ const Documentation = () => {
         </a>
         <a href={lettre} download>
           <DocumentCard title="Lettre de candidature" date="14/05/2021" />
+        </a>
+        <a href={ensemble} download>
+          <DocumentCard title="Ensemble continuons" date="10/06/2021" />
         </a>
       </div>
     </StyledDocumentation>
