5169d57499aba943af5a9e9df6ecf00e84069a15
Adrien Barbier <abarbier@debutant-e.pro>
2021-06-19 07:37:08 +0200

    (feat) - maintenance message

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { SnackbarProvider } from 'notistack'
 import Layout from './src/components/Layout/Layout'
+import { StyledMaintenance } from './src/styles/maintenanceStyles'
 
 export const wrapRootElement = ({ element }) => {
   return (
-    <div></div>
+    <StyledMaintenance>
+      Site temporairement indisponible
+    </StyledMaintenance>
     // <Layout>
     //   <SnackbarProvider maxSnack={3}>{element}</SnackbarProvider>
     // </Layout>
